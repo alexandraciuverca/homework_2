25fa3c14951aecbe5a8c71966a8631472b4a75a0
Ubuntu <ubuntu@ip-172-31-32-102.us-east-2.compute.internal>
2019-11-19 17:24:28 +0000

    https://github.com/alexandraciuverca/homework_2

--- main/app.js
+++ main/app.js
@@ -1,5 +1,35 @@
 function addTokens(input, tokens){
     
+    
+   
+    if((input)&& (typeof input !='string'))
+    {
+        throw new Error("Invalid input");
+    }
+     if(input.length < 6){
+        throw new Error("Input should have at least 6 characters")
+    }
+    
+    for(let i=0; i<tokens.length;i++)
+    {
+        if(typeof( tokens[i].tokenName )!='string')
+        {
+            throw new Error("Invalid array format");
+        }
+        
+    }    
+    if (!input.includes("...")) 
+    {return input;}
+    
+    if(input.includes("...")){
+         for(let i=0; i<tokens.length;i++){
+            input=input.replace("...","${"+Object.values(tokens[i])+"}")
+            return input;
+         }
+    }
+    
+   
+
 }
 
 const app = {
